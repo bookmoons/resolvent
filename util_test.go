9b37fc886d1f282121f4d88c1ade9f09e1674c46
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-08-18 00:40:37 -0400

    Test isIPv6

--- util_test.go
+++ util_test.go
@@ -33,3 +33,32 @@ func TestIsIPv4(t *testing.T) {
 		})
 	}
 }
+
+func TestIsIPv6(t *testing.T) {
+	t.Parallel()
+	pass := []net.IP{
+		net.ParseIP("2001:db8::1"),
+		net.ParseIP("2001:db8::88"),
+	}
+	fail := []net.IP{
+		net.ParseIP("192.0.2.1"),
+		net.ParseIP("198.51.100.1"),
+		net.ParseIP("203.0.113.1"),
+		net.ParseIP("::ffff:192.0.2.2"),
+		[]byte{1, 2, 3, 4, 5},
+	}
+	for _, item := range pass {
+		t.Run(item.String(), func(t *testing.T) {
+			if !isIPv6(item) {
+				t.Errorf("incorrect fail")
+			}
+		})
+	}
+	for _, item := range fail {
+		t.Run(item.String(), func(t *testing.T) {
+			if isIPv6(item) {
+				t.Errorf("incorrect pass")
+			}
+		})
+	}
+}
