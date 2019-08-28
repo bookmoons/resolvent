34129876809e0fbaf05b4f524e7186e7cb29fc92
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-08-28 01:36:33 -0400

    Document querier/network

--- querier/network/network.go
+++ querier/network/network.go
@@ -1,3 +1,4 @@
+// Package network implements a querier that performs network exchange.
 package network
 
 import (
@@ -11,6 +12,7 @@ type networkQuerier struct {
 	client *dns.Client
 }
 
+// New returns a querier that performs network exchange.
 func New() *networkQuerier {
 	client := new(dns.Client)
 	return &networkQuerier{
@@ -18,6 +20,7 @@ func New() *networkQuerier {
 	}
 }
 
+// Query executes an exchange with a single DNS nameserver.
 func (q *networkQuerier) Query(
 	ctx context.Context,
 	address net.IP,
